c9f6e6284f559161380faf27225c7131beba7d52
JOAO MARCOS VIRGINO DE MESQUITA <49624480+joaomarcos08@users.noreply.github.com>
2022-07-23 13:58:08 -0300

    Add files via upload

---
+++ Crud/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Crud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.studentId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fullname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Birthday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Emmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.nome = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.aniversario = new System.Windows.Forms.MaskedTextBox();
+            this.endereco = new System.Windows.Forms.TextBox();
+            this.masculino = new System.Windows.Forms.CheckBox();
+            this.feminino = new System.Windows.Forms.CheckBox();
+            this.editar = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.cancelar = new System.Windows.Forms.Button();
+            this.salvar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.studentId,
+            this.Fullname,
+            this.Gender,
+            this.Birthday,
+            this.Emmail,
+            this.Address});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(465, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // studentId
+            // 
+            this.studentId.HeaderText = "Student ID";
+            this.studentId.Name = "studentId";
+            // 
+            // Fullname
+            // 
+            this.Fullname.HeaderText = "Fullname";
+            this.Fullname.Name = "Fullname";
+            // 
+            // Gender
+            // 
+            this.Gender.HeaderText = "Gender";
+            this.Gender.Name = "Gender";
+            // 
+            // Birthday
+            // 
+            this.Birthday.HeaderText = "Birthday";
+            this.Birthday.Name = "Birthday";
+            // 
+            // Emmail
+            // 
+            this.Emmail.HeaderText = "Email";
+            this.Emmail.Name = "Emmail";
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox1.Location = new System.Drawing.Point(490, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(525, 238);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 2;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(656, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Student ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(659, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fullname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(659, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Gender:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(659, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Birthday:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(659, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(659, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Address:";
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(724, 82);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(100, 20);
+            this.id.TabIndex = 9;
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(724, 109);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(100, 20);
+            this.nome.TabIndex = 10;
+            this.nome.TextChanged += new System.EventHandler(this.nome_TextChanged);
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(724, 190);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(100, 20);
+            this.email.TabIndex = 12;
+            this.email.TextChanged += new System.EventHandler(this.email_TextChanged);
+            this.email.Validated += new System.EventHandler(this.email_Validated);
+            // 
+            // aniversario
+            // 
+            this.aniversario.Location = new System.Drawing.Point(724, 156);
+            this.aniversario.Mask = "00/00/0000";
+            this.aniversario.Name = "aniversario";
+            this.aniversario.Size = new System.Drawing.Size(100, 20);
+            this.aniversario.TabIndex = 14;
+            this.aniversario.ValidatingType = typeof(System.DateTime);
+            this.aniversario.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.aniversario_MaskInputRejected);
+            // 
+            // endereco
+            // 
+            this.endereco.Location = new System.Drawing.Point(724, 219);
+            this.endereco.Multiline = true;
+            this.endereco.Name = "endereco";
+            this.endereco.Size = new System.Drawing.Size(137, 64);
+            this.endereco.TabIndex = 15;
+            this.endereco.TextChanged += new System.EventHandler(this.endereco_TextChanged);
+            // 
+            // masculino
+            // 
+            this.masculino.AutoSize = true;
+            this.masculino.Location = new System.Drawing.Point(724, 133);
+            this.masculino.Name = "masculino";
+            this.masculino.Size = new System.Drawing.Size(74, 17);
+            this.masculino.TabIndex = 16;
+            this.masculino.Text = "Masculino";
+            this.masculino.UseVisualStyleBackColor = true;
+            // 
+            // feminino
+            // 
+            this.feminino.AutoSize = true;
+            this.feminino.Location = new System.Drawing.Point(804, 133);
+            this.feminino.Name = "feminino";
+            this.feminino.Size = new System.Drawing.Size(68, 17);
+            this.feminino.TabIndex = 17;
+            this.feminino.Text = "Feminino";
+            this.feminino.UseVisualStyleBackColor = true;
+            // 
+            // editar
+            // 
+            this.editar.Location = new System.Drawing.Point(543, 313);
+            this.editar.Name = "editar";
+            this.editar.Size = new System.Drawing.Size(75, 23);
+            this.editar.TabIndex = 19;
+            this.editar.Text = "Edit";
+            this.editar.UseVisualStyleBackColor = true;
+            this.editar.Click += new System.EventHandler(this.editar_Click);
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(624, 313);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(75, 23);
+            this.del.TabIndex = 20;
+            this.del.Text = "Delete";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // cancelar
+            // 
+            this.cancelar.Location = new System.Drawing.Point(705, 313);
+            this.cancelar.Name = "cancelar";
+            this.cancelar.Size = new System.Drawing.Size(75, 23);
+            this.cancelar.TabIndex = 21;
+            this.cancelar.Text = "Cancel";
+            this.cancelar.UseVisualStyleBackColor = true;
+            this.cancelar.Click += new System.EventHandler(this.cancelar_Click);
+            // 
+            // salvar
+            // 
+            this.salvar.Location = new System.Drawing.Point(786, 313);
+            this.salvar.Name = "salvar";
+            this.salvar.Size = new System.Drawing.Size(75, 23);
+            this.salvar.TabIndex = 22;
+            this.salvar.Text = "Save";
+            this.salvar.UseVisualStyleBackColor = true;
+            this.salvar.Click += new System.EventHandler(this.salvar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 369);
+            this.Controls.Add(this.salvar);
+            this.Controls.Add(this.cancelar);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.editar);
+            this.Controls.Add(this.feminino);
+            this.Controls.Add(this.masculino);
+            this.Controls.Add(this.endereco);
+            this.Controls.Add(this.aniversario);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.MaskedTextBox aniversario;
+        private System.Windows.Forms.TextBox endereco;
+        private System.Windows.Forms.CheckBox masculino;
+        private System.Windows.Forms.CheckBox feminino;
+        private System.Windows.Forms.Button editar;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button cancelar;
+        private System.Windows.Forms.Button salvar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fullname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Birthday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Emmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+    }
+}
+
---
+++ Crud/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Crud
+{
+    public partial class Form1 : Form
+    {
+        string caminho;
+        string genero;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection c = new SqlConnection(@"Data Source=snbbllab02-12\joao;Initial Catalog=crud;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            id.Enabled = false;
+            nome.Enabled = false;
+            masculino.Enabled = false;
+            feminino.Enabled = false;
+            aniversario.Enabled = false;
+            email.Enabled = false;
+            endereco.Enabled = false;
+
+
+            salvar.Enabled = false;
+            editar.Enabled = false;
+            del.Enabled = false;
+            gridData();
+        }
+
+        private void validar()
+        {
+            if(id.Text != "" && nome.Text != "" && aniversario.Text != "" && email.Text !="" && endereco.Text != "" && pictureBox1.ImageLocation != "" )
+            {
+                editar.Enabled = true;
+                del.Enabled = true;
+            } else
+            {
+                editar.Enabled = false;
+                del.Enabled = false;
+            }
+        }
+
+        private void validar1()
+        {
+            if (nome.Text != "" && aniversario.Text != "" && email.Text != "" && endereco.Text != "" && pictureBox1.ImageLocation != "")
+            {
+                salvar.Enabled = true;
+            }
+        }
+        private void gridData()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+
+            int i = 0;
+
+            c.Open();
+
+            SqlCommand cmd = new SqlCommand("select id, nome, genero, aniversario, email, endereco from Estudante ", c);
+            SqlDataReader fill = cmd.ExecuteReader();
+
+            while (fill.Read())
+            {
+                dataGridView1.Rows.Add();
+
+                dataGridView1.Rows[i].Cells[0].Value = fill[0].ToString();
+                dataGridView1.Rows[i].Cells[1].Value = fill[1].ToString();
+                dataGridView1.Rows[i].Cells[2].Value = fill[2].ToString();
+                dataGridView1.Rows[i].Cells[3].Value = fill[3].ToString();
+                dataGridView1.Rows[i].Cells[4].Value = fill[4].ToString();
+                dataGridView1.Rows[i].Cells[5].Value = fill[5].ToString();
+                i++;
+
+            }
+
+
+            c.Close();
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void browse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                caminho = openFileDialog1.FileName;
+                pictureBox1.ImageLocation = caminho;
+
+
+                nome.Enabled = true;
+                masculino.Enabled = true;
+                feminino.Enabled = true;
+                aniversario.Enabled = true;
+                email.Enabled = true;
+                endereco.Enabled = true;
+            }
+        }
+
+        private void salvar_Click(object sender, EventArgs e)
+        {
+            
+            byte[] images = null;
+
+            FileStream stream = new FileStream(caminho, FileMode.Open, FileAccess.Read);
+
+            BinaryReader ImagemConvertido = new BinaryReader(stream);
+
+            images = ImagemConvertido.ReadBytes((int)stream.Length);
+
+            c.Open();
+
+            if (masculino.Checked)
+            {
+                genero = "masculino";
+            }else if (feminino.Checked)
+            {
+                genero = "feminino";
+            }
+
+            SqlCommand inserir = new SqlCommand("insert into Estudante(nome,genero,aniversario,email,endereco,foto) values('" + nome.Text + "','" + genero + "','" + aniversario.Text + "','"+email.Text+"','"+endereco.Text+"',@i)", c);
+            inserir.Parameters.Add("@i", images);
+            int r = inserir.ExecuteNonQuery();
+            if (r == 1)
+            {
+                MessageBox.Show("Aluno Cadastrado");
+                c.Close();
+                gridData();
+                c.Open();
+
+            }
+            else
+            {
+                MessageBox.Show("Erro no Cadastro");
+            }
+            c.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string iid = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            
+            c.Open();
+            SqlCommand cmd = new SqlCommand("select * from Estudante where id = '" + iid + "'", c);
+            SqlDataReader fill = cmd.ExecuteReader();
+
+            if (fill.Read())
+            {                
+                id.Text = fill[0].ToString();
+                nome.Text = fill[1].ToString();
+                string genero = fill[2].ToString();
+                aniversario.Text = fill[3].ToString();
+                email.Text = fill[4].ToString();
+                endereco.Text = fill[5].ToString();
+
+                byte[] images = ((byte[])fill[6]);
+                MemoryStream mstreem = new MemoryStream(images);
+                pictureBox1.Image = Image.FromStream(mstreem);
+
+
+
+                if (genero == "masculino")
+                {
+                    masculino.Checked = true;
+                } else if (genero == "feminino")
+                {
+                    feminino.Checked = true;
+                }
+            }
+
+            c.Close();
+            validar();
+            nome.Enabled = true;
+            masculino.Enabled = true;
+            feminino.Enabled = true;
+            aniversario.Enabled = true;
+            email.Enabled = true;
+            endereco.Enabled = true;
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            c.Open();
+            SqlCommand cmd = new SqlCommand("delete from Estudante where id = '" + id.Text + "'", c);
+            int result = cmd.ExecuteNonQuery();
+             if (result == 1)
+            {
+                MessageBox.Show("cadastro deletado com sucesso");
+                c.Close();
+                gridData();
+                c.Open();
+                Limpar();
+            }
+            else if (result == 0)
+            {
+                MessageBox.Show("Erro ao deletar cadastro");
+            }
+            c.Close();
+        }
+
+        private void Limpar()
+        {
+            id.Clear();
+            nome.Clear();
+            masculino.Checked = false;
+            feminino.Checked = false;
+            aniversario.Clear();
+            email.Clear();
+            endereco.Clear();
+        }
+        private void cancelar_Click(object sender, EventArgs e)
+        {
+            Limpar();
+            validar();
+            id.Enabled = false;
+            nome.Enabled = false;
+            masculino.Enabled = false;
+            feminino.Enabled = false;
+            aniversario.Enabled = false;
+            email.Enabled = false;
+            endereco.Enabled = false;
+
+
+            salvar.Enabled = false;
+            editar.Enabled = false;
+            del.Enabled = false;
+
+            pictureBox1.Image = null;
+
+        }
+
+        private void editar_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Deseja realizar as alterações no cadastro?", "confirmar", MessageBoxButtons.YesNo);
+            //DialogResult resulta;
+            //if (resulta == DialogResult.Yes)
+            //{
+
+            //}
+
+            if (masculino.Checked)
+            {
+                genero = "masculino";
+            }
+            else if (feminino.Checked)
+            {
+                genero = "feminino";
+            }
+
+            string message = "Deseja realizar as alterações no cadastro";
+            string caption = "confirmar";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult resulta;
+
+            resulta = MessageBox.Show(message, caption, buttons);
+            if (resulta == System.Windows.Forms.DialogResult.Yes)
+            {
+            c.Open();
+            SqlCommand cmd = new SqlCommand("update Estudante set nome = '"+nome.Text+"', '"+genero+"','"+aniversario.Text+"','"+endereco.Text+"' where id = '" + id.Text + "'",c);
+            int result = cmd.ExecuteNonQuery();
+            c.Close();
+            }
+        }
+
+        private void email_Validated(object sender, EventArgs e)
+        {
+            int posicao, posicao2;
+            posicao = email.Text.IndexOf("@");
+            posicao2 = email.Text.IndexOf(".com");
+            if (posicao == -1 || posicao2 == -1)
+            {
+                MessageBox.Show("Email Invalida");
+                email.BackColor = Color.Red;
+            }
+            else
+            {
+                email.BackColor = Color.Green;
+            }
+        }
+
+        private void nome_TextChanged(object sender, EventArgs e)
+        {
+            validar1();
+        }
+
+        private void aniversario_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            validar1();
+        }
+
+        private void email_TextChanged(object sender, EventArgs e)
+        {
+            validar1();
+        }
+
+        private void endereco_TextChanged(object sender, EventArgs e)
+        {
+            validar1();
+        }
+    }
+}
